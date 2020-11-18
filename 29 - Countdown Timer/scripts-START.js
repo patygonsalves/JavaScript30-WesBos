9b3b6f523f039f454b51c4e0c60b23b5aed0670e
Patricia <patricia.gonsalves@hiplatform.com.br>
2020-11-18 19:30:50 -0300

    Javascript Course by WesBos

---
+++ 29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,59 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left')
+const endTime = document.querySelector('.display__end-time')
+const buttons = document.querySelectorAll('[data-time]')
+
+
+function timer(seconds) {
+  // clear any existing timers
+  clearInterval(countdown);
+
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check if we should stop it!
+    if(secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    // display it
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60)
+  const remainderSeconds = seconds % 60
+  
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  document.title = display
+  timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp)
+  const hour = end.getHours()
+  const ajustedHour = hour > 12 ? hour - 12 : hour
+  const minutes = end.getMinutes()
+
+  endTime.textContent = `Be back at ${ajustedHour}:${minutes < 10 ? '0' : ''}${minutes}`
+}
+
+function startTime() {
+  const seconds = parseInt(this.dataset.time)
+  timer(seconds)
+}
+
+function startInputTime(e) {
+  e.preventDefault()
+  const seconds = this.minutes.value * 60;
+  timer(seconds)
+  this.reset()
+}
+
+buttons.forEach(button => button.addEventListener('click', startTime))
+document.customForm.addEventListener('submit', startInputTime)
